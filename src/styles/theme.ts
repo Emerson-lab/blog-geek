310541840028428c6f9725b88aba253b951977a0
Emerson Trindade <trinddade29@gmail.com>
2022-05-14 03:33:34 -0300

    scrollbar

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,22 @@
-import {extendTheme} from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools'
+import { Dict } from '@chakra-ui/utils';
 
-const customTheme = {}
+export const theme = extendTheme({
+  styles: {
+    global: (props: Dict<any>) => ({
+      "::-webkit-scrollbar": {
+        width: "9px",
+        height: "9px"
+      },
+      "::-webkit-scrollbar-track": {
+        background: mode("#b3b3b3", "gray.800")(props),
+        border: "0px #fff"
+      },
+      "::-webkit-scrollbar-thumb": {
+        background: mode("#feb2b2", "#feb2b2")(props)
+      }
+    })
+  }
+});
 
-const theme = extendTheme(customTheme);
-
-export default theme;
